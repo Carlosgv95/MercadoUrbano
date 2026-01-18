d77ce449f5338786e7206eb55e9ceea0607a3f0e
heberto rivas <hebertorivas.1991@gmail.com>
2026-01-17 23:55:47 -0300

    Agregar contador de carrito en Navbar y conectar ProductSlider con CartContext

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,9 +4,16 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import NavbarBS from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext'; // 👈 importa tu contexto
 
 const Navbar = () => {
+  const { cartItems } = useContext(CartContext); // 👈 accede al carrito
+
+  // Calcular cantidad total de productos
+  const cartCount = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <NavbarBS expand="lg" bg="success" data-bs-theme="dark">
       <Container fluid>
@@ -18,20 +25,23 @@ const Navbar = () => {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link as={Link} to="/ingreso"> Ingresar 🔐 </Nav.Link>
-            <Nav.Link as={Link} to="/registro"> Registrate 🔐</Nav.Link>
+            <Nav.Link as={Link} to="/ingreso">Ingresar 🔐</Nav.Link>
+            <Nav.Link as={Link} to="/registro">Registrate 🔐</Nav.Link>
             <NavDropdown title="Categorias" id="navbarScrollingDropdown">
               <NavDropdown.Item as={Link} to="/productos">Productos</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Servicios
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Servicios</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Contactenos
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">Contactenos</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link as={Link} to="/carrito"> 🛒 </Nav.Link>
+
+            {/* 🛒 Carrito con contador */}
+            <Nav.Link as={Link} to="/carrito">
+              🛒 {cartCount > 0 && (
+                <span className="badge bg-warning text-dark ms-1">{cartCount}</span>
+              )}
+            </Nav.Link>
           </Nav>
+
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -45,6 +55,6 @@ const Navbar = () => {
       </Container>
     </NavbarBS>
   );
-}
+};
 
 export default Navbar;
