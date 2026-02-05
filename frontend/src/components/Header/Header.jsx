ba516a6cf5f2bbf8c9b2d4c04cbd653a6897a5f6
heberto rivas <hebertorivas.1991@gmail.com>
2026-02-05 20:38:21 -0300

    Volviendo a mejorar el Header, ya que el anterior no se ve bien

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,43 +8,37 @@ const slide3 = "https://compartirenfamilia.com/wp-content/uploads/2024/04/GettyI
 const Header = () => {
   return (
     <Carousel fade interval={4000} className="custom-carousel">
-      {/* SLIDE 1: LOGO */}
       <Carousel.Item>
-        <div className="carousel-overlay"></div>
         <img
           className="d-block w-100 carousel-image"
           src={slide1}
           alt="Logo MercadoUrbano"
         />
-        <Carousel.Caption className="carousel-caption">
+        <Carousel.Caption>
           <h2 className="carousel-title">Bienvenido a MercadoUrbano</h2>
           <p className="carousel-text">Tu mercado digital de confianza</p>
         </Carousel.Caption>
       </Carousel.Item>
 
-      {/* SLIDE 2 */}
       <Carousel.Item>
-        <div className="carousel-overlay"></div>
         <img
           className="d-block w-100 carousel-image"
           src={slide2}
           alt="Ofertas especiales"
         />
-        <Carousel.Caption className="carousel-caption">
+        <Carousel.Caption>
           <h2 className="carousel-title">Ofertas Especiales</h2>
           <p className="carousel-text">Hasta 50% de descuento por tiempo limitado</p>
         </Carousel.Caption>
       </Carousel.Item>
 
-      {/* SLIDE 3 */}
       <Carousel.Item>
-        <div className="carousel-overlay"></div>
         <img
           className="d-block w-100 carousel-image"
           src={slide3}
           alt="Tecnología y más"
         />
-        <Carousel.Caption className="carousel-caption">
+        <Carousel.Caption>
           <h2 className="carousel-title">Tecnología, Hogar y Más</h2>
           <p className="carousel-text">Encuentra todo lo que necesitas en un solo lugar</p>
         </Carousel.Caption>
